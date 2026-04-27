d7e36b1b85a7cbf2bbb77b0083452b16c604f2ef
Mickaël <150048395+Micka1310@users.noreply.github.com>
2026-04-28 00:37:07 +0200

    Project Initialisation (#9)

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LibFile;
+
+Library class1 = new Library();
+class1.Test1();
---
+++ Lib/Library.cs
@@ -0,0 +1,16 @@
+﻿namespace LibFile;
+
+public class Library
+{
+    public void Test1()
+    {
+        Console.WriteLine("Ca marche !");
+    }
+
+    public int Test2(int a, int b)
+    {
+        int sum = a + b;
+
+        return sum;
+    }
+}
